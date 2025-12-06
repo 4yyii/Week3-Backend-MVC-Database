77deae26843856b3dcbcad4d0501aec746db5adb
Ayi <arifinrafli502@gmail.com>
2025-12-07 05:20:05 +0700

    upload week 3

---
+++ logic_nolep/warehouse-sql/insert_inventory.sql
@@ -0,0 +1,5 @@
+INSERT INTO Inventory VALUES
+(1, 1, 50, 'Warehouse A'),
+(2, 2, 30, 'Warehouse B'),
+(3, 3, 20, 'Warehouse A'),
+(4, 4, 40, 'Warehouse B');
---
+++ logic_nolep/warehouse-sql/insert_orders.sql
@@ -0,0 +1,9 @@
+INSERT INTO Orders VALUES
+(1, 101, '2024-08-12'),
+(2, 102, '2024-08-13');
+
+INSERT INTO OrderDetails VALUES
+(1, 1, 1, 2),
+(2, 1, 3, 1),
+(3, 2, 2, 1),
+(4, 2, 4, 2);
